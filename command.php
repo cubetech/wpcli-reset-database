e4164d3401e91566be39458952b0c315c87b1a7f
Marc <marc.mentha@cubetech.ch>
2017-10-31 14:19:08 +0100

    changed code

--- command.php
+++ command.php
@@ -9,19 +9,49 @@ if (! class_exists('WP_CLI')) {
  *
  * @when before_wp_load
  */
-$ct_reset_database = function () {
-	
-	$config = WP_CLI::get_config();
-	$configPath = $config['path'] . 'wp-content/DB_Reset/';
-	$baseDbName = 'baseDbBackup.sql';
-	
-	if (! file_exists($configPath)) {
-		echo 'initialize DB-Reset folder in wp-content directory';
-		exec('mkdir ' . $configPath);
-		WP_CLI::runcommand('db export ' . $configPath . $baseDbName);
-	} else {
-		WP_CLI::runcommand('db import ' . $configPath . $baseDbName);
-		WP_CLI::success("Database successfully reseted");
+class ResetDB
+{
+
+	public function createDump()
+	{
+		$config = $this->getconfig();
+		if (!file_exists($config->path)) {
+			exec('mkdir ' . $configPath);
+		}
+		WP_CLI::runcommand('db export ' . $config->path . $config->file);
+		WP_CLI::success('Created Database-Dump in ' . $config->path . $config->file);
 	}
-};
-WP_CLI::add_command('ct-reset-database', $ct_reset_database);
+
+	public function resetInstallation()
+	{
+		$config = $this->getConfig();
+
+		if (!file_exists($config->path . $config->file)) {
+			WP_CLI::error('No basedump found. Run "wp ct create-dump" and try again');
+			return 0;
+		}
+
+		WP_CLI::runcommand('db import ' . $config->path . $config->file);
+		WP_CLI::success("Database successfully reseted");
+
+		//send mail
+		//reset media
+		//delete cache?
+		//reset plugin / theme files?
+	}
+
+	private function getConfig()
+	{
+
+		$tmp = WP_CLI::get_config();
+		$config = new stdClass();
+		$config->path = $tmp['path'] . 'wp-content/ct-dump/';
+		$config->file = 'base-db-dump.sql';
+		return $config;
+	}
+}
+
+$resetCommand = new ResetDB();
+
+WP_CLI::add_command('ct create-dump', [$resetCommand, 'createDump']);
+WP_CLI::add_command('ct reset-database', [$resetCommand, 'resetInstallation']);
